d5e95651cc45ae5793bdf8f531a787ab2af232a5
mihai ciurca <Mihai.Ciurca@orange.com>
2019-09-29 17:14:50 +0300

    replaced url

--- src/app/category.service.ts
+++ src/app/category.service.ts
@@ -3,21 +3,20 @@ import {Injectable} from '@angular/core';
 import { Category} from './category';
 import {HttpClient} from '@angular/common/http';
 import {Product} from './product';
+import {environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  configUrl = 'http://192.168.1.213:8080';
-
   constructor(private httpClient: HttpClient) { }
 
   getProductFromCategory(selectedCategory: number) {
-    return this.httpClient.get<Product[]>(this.configUrl + '/category/' + selectedCategory);
+    return this.httpClient.get<Product[]>(environment.backendUrl + '/category/' + selectedCategory);
   }
 
   getCategories(selectedRestaurant: string) {
-    return this.httpClient.get<Category[]>(this.configUrl + '/restaurant/' +  selectedRestaurant + '/category');
+    return this.httpClient.get<Category[]>(environment.backendUrl + '/restaurant/' +  selectedRestaurant + '/category');
   }
 }
