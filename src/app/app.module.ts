5818c9c6a31f0cd3389307a34e403c92a105aef2
HappyNightQQ <blajalexandru16@gmail.com>
2019-09-29 18:13:05 +0300

    toolbar css needed category layout working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CategoryComponent } from './category/category.component';
 import { RouterModule } from '@angular/router';
 import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatCardModule, MatCheckboxModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
 import {HttpClientModule} from '@angular/common/http';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategoryComponent
+    CategoryComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +28,7 @@ import {HttpClientModule} from '@angular/common/http';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+  title: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Virtual menu';
+  }
+
+  enterMenu(): void {
+  }
+
+  enterMainPage(): void {
+  }
+
+  enterShoppingCart(): void {
+  }
+}
