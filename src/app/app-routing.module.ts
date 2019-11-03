d1dec560963af037e8933745bf572a7fead5020e
HappyNightQQ <blajalexandru16@gmail.com>
2019-11-03 16:23:45 +0200

    Merge branch 'shopping-cart' of https://github.com/TheRealHappyNight/virtualMenuFE into Logout
     Conflicts:
    	src/app/admin-page/products/admin-page-products.component.html
    	src/app/app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
-import {AdminPageProductsComponent} from './admin-page/products/admin-page-products.component';
 import {CategoryListingComponent} from './category-listing/category-listing.component';
 import {LoginComponent} from './auth/login/login.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {ProductListingComponent} from './product-listing/product-listing.component';
-import {CartViewComponent} from 'ng-shopping-cart';
 import {CartListingComponent} from './cart-listing/cart-listing.component';
+import {AdminPageComponent} from './admin-page/admin-page.component';
 
 const appRoutes: Routes = [
   {path: '', component: CategoryListingComponent},
-  {path: 'admin', component: AdminPageProductsComponent},
+  {path: 'admin', component: AdminPageComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: RegisterComponent},
   {path: 'category/:id', component: ProductListingComponent},
